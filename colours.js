1a7cfad88301d4db61440219ececefcbe5b3aac3
blurringtheboundaries <58079736+blurringtheboundaries@users.noreply.github.com>
2022-07-13 10:12:24 -0400

    Add files via upload

---
+++ colours.js
@@ -0,0 +1,32 @@
+/*
+Colour	            Note
+deep red	        F
+red	                C
+orange	            G
+yellow	            D
+green	            A
+sky blue	        E
+blue	            B
+bright blue	        F♯
+violet or purple	C♯
+lilac	            G♯
+flesh	            D♯
+rose	            A♯
+*/
+
+/*
+
+red	                C
+violet or purple	C♯
+yellow	            D
+flesh	            D♯
+sky blue	        E
+deep red	        F
+bright blue	        F♯
+orange	            G
+lilac	            G♯
+green	            A
+rose	            A♯
+blue	            B
+
+*/
