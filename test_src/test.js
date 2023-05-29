c59b5ef3624bfb206e10af13ff1b51c7c4854838
Charles Matthews <charles0matthews@gmail.com>
2023-05-29 18:17:52 -0400

    add a voice management class, tweak build script

--- test_src/test.js
+++ test_src/test.js
@@ -3,12 +3,14 @@ import SocketMapper from '@matthewscharles/socket-mapper';
 import SerialMapper from 'serial-mapper';
 import { dmxWrite } from './dmx.js';
 import { noteColours, COLOURS } from '../src/synth_colours.js';
+import VoiceManager from './voice.js';
 
 window.MidiMapper = MidiMapper;
 window.colours = {
     midi: new MidiMapper(),
     socket: new SocketMapper(),
-    serial: new SerialMapper()  
+    arduino: new SerialMapper(),
+    voices: new VoiceManager(4)  
 }
 
 console.log('test.js loaded');
@@ -30,12 +32,13 @@ window.start = function start(){
  */
 
 function testCycle(){
+    let {arduino} = colours;
     let count = 0, direction = 1;
-    serial.writer.write('1 255\n');
+    arduino.writer.write('1 255\n');
     window.setInterval(()=>{
-       serial.writer.write(`2 ${Math.floor(Math.abs(Math.sin(count/100)*255))}\n`);
+       arduino.writer.write(`2 ${Math.floor(Math.abs(Math.sin(count/100)*255))}\n`);
         // console.log(Math.floor(Math.abs(Math.sin(count/100)*255)))
-       serial.writer.write(`3 ${count%255}\n`);
+       arduino.writer.write(`3 ${count%255}\n`);
        count += direction;
        if(count == 255){direction = -1}
        if(count == 0){direction = 1}
@@ -43,13 +46,15 @@ function testCycle(){
 }
 
 function initSocket(){
+    let {socket} = colours;
     let synth = new Tone.PolySynth().toMaster();
     Tone.start();
     console.log('init')
-    colours.socket.listen();
-    colours.socket.in.note = (e)=>{
+    socket.listen();
+    socket.in.note = (e)=>{
         let [channel, pitch, velocity] = e;
         synth[e[2] ? 'triggerAttack' : 'triggerRelease'](Tone.Frequency(pitch, 'midi').toNote(), undefined, velocity/127);
-    }   //- colours.socket.listen();
+    } 
 }
+
 document.querySelectorAll('#socketInit').forEach(x=>x.addEventListener('click', initSocket));
