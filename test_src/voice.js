a015771b89cb9a22fccb7a7dfcb28842814c4daa
Charles Matthews <charles0matthews@gmail.com>
2023-05-30 16:52:35 -0400

    Update voice.js

--- test_src/voice.js
+++ test_src/voice.js
@@ -4,15 +4,27 @@
 
 class Voice{
     constructor(){
-
+        Object.assign(this,{
+            pitch:-1,
+            velocity:0,
+            active:false
+        })
     }
 
-    on(velocity){
-    
+    on(pitch, velocity){
+        this.active = true;
+        Object.assign(this,{
+            pitch,
+            velocity
+        })
     }
     
     off(){
-
+        this.active = false;
+        Object.assign(this,{
+            pitch:-1,
+            velocity:0
+        })
     }
 }
 
@@ -33,7 +45,27 @@ export default class VoiceManager{
     }
 
     update(pitch, velocity){
-
+        let activeVoices = this.voices.filter(v=>v.active);
+        let voice = this.voices.find(v=>v.pitch == pitch);
+        if(velocity){
+            if(voice){
+                voice.on(velocity);
+            }else{
+                if(activeVoices.length == this.voices.length){
+                    if(this.stealing){
+                        this.steal(pitch, velocity);
+                    }
+                }
+                else{
+                    this.voices[this.voices.length-1].on(velocity);
+                }
+            }
+        } else {
+            if(voice){
+                voice.off();
+            }
+        }
+        
     }
 
 
