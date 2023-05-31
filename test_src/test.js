de899fc715f326f7458695dd7b3e3bba05af0e83
Charles Matthews <charles0matthews@gmail.com>
2023-05-31 15:59:44 -0400

    troubleshooting dmx, nearly there

--- test_src/test.js
+++ test_src/test.js
@@ -17,11 +17,17 @@ window.colours = {
     initFlag: false,
     polyphony: 4,
     lights:{
-        0:[0,0,0],
         1:[0,0,0],
         2:[0,0,0],
-        3:[0,0,0]
+        3:[0,0,0],
+        4:[0,0,0]
     }, 
+    lights_last:{
+        1:[0,0,0],
+        2:[0,0,0],
+        3:[0,0,0],
+        4:[0,0,0]
+    },
     queue: [],
 }
 
@@ -67,20 +73,12 @@ function getColour(array, index){
 }
 
 function formatColour(array, offset = 0){
-    offset += 2;
+    offset += 1;
     return array.map((x,i)=>`${i+offset}:${x}`).join('\n') + `\n${offset - 1}:255\n`;
 }
 
 function writeNoteColour(note = 0, offset = 0){
     let {arduino, initFlag} = colours;
-    if(!initFlag){
-        for (let i=0; i<colours.polyphony; i++){
-
-            console.log('init ', 1 + i * 7)
-            arduino.writer.write(`${1 + i * 7}:255\n`);
-        }
-        colours.initFlag = true;
-    }
     let colour;
     if(note == -1){
         colour = [0,0,0];
@@ -90,32 +88,31 @@ function writeNoteColour(note = 0, offset = 0){
     }
     console.log('colour',offset, colour);
 
-    
-    // if(colours.lights[offset].join('') == colour.join('')){
-    //     return;
-    // } else {
-    //     colours.queue.push(formatColour(colour, offset * 6));
-
-    // }
-
-    colours.lights[offset] = colour;
-    // console.log(formatColour(colour, offset * 7).split('\n'));
-    // todo: try writeQueue in the SerialMapper instance
-    // console.log(formatColour(colour, offset * 6));
-    // arduino.writer.write(formatColour(colour, offset * 6));
-}
+    colours.lights[offset+1] = colour;
+    processAll();
+ }
 
 function processAll(){
     let {queue, arduino, lights} = colours;
-    console.log(lights);
+    // console.log(lights);
     colours.queue = [];
-    Object.entries(lights).forEach(([offset, colour])=>{
-        colours.queue.push(formatColour(colour, offset * 7));
+    Object.entries(lights).forEach(([index, colour])=>{
+        // console.log(colour)
+        // console.log('test',colours.lights_last)
+        // if(!colour.length) return;
+        // console.log('join1',colour.join(''))
+        // console.log('test2222', colours.lights_last[index], index)
+        if(colour.join('') == colours.lights_last[index].join('')){
+        } else {
+            colours.queue.push(formatColour(colour, index));
+            colours.lights_last[index] = colour;
+        }
+        
     })
     console.log(queue);
     arduino.writeQueue(colours.queue);
     queue = [];
-    console.log(queue);
+    // console.log(queue);
 }
 
 
