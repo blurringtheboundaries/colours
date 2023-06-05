fb87e55507d1131931ea5040c217303ddc9c0c45
Charles Matthews <charles0matthews@gmail.com>
2023-06-05 09:03:07 -0400

    fix order of declarations

--- test_src/test.js
+++ test_src/test.js
@@ -137,15 +137,14 @@ function writeNoteColour(note = 0, offset = 0){
  }
 
 function processAll(){
-    let duck
+    let {queue, arduino, lights, audio} = colours;
+    
+    let duck;
     if(audio.meter){
         duck = Tone.dbToGain(audio.meter.getLevel())*1;
         if(duck > 1){ duck=1};
     }
-    let {queue, arduino, lights, audio} = colours;
-    
-    
-    
+        
     colours.queue = [];
     Object.entries(lights).forEach(([index, colour])=>{
         if(colour.join('') == colours.lights_last[index].join('')){
@@ -159,7 +158,7 @@ function processAll(){
     console.log(queue);
     arduino.writeQueue(colours.queue);
     queue = [];
-    // console.log(queue);
+
 }
 
 
