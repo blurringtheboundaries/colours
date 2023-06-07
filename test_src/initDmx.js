fe4408c886347e52067ef83cf373b27a7bd99afd
Charles Matthews <charles0matthews@gmail.com>
2023-06-07 09:18:16 -0400

    sustain working

--- test_src/initDmx.js
+++ test_src/initDmx.js
@@ -1,14 +1,28 @@
 function initDMX(){
     let {arduino, socket, voices} = window.colours;
     arduino.connect();
-    socket.in.cc = (e) => {console.log(e)}
+    socket.in.cc = (e) => {
+        let [channel, cc, value] = e;
+        if(cc == 64){
+            if(value == 127){
+                colours.hold = true;
+            } else if(colours.hold) {
+                voices.flush();
+                colours.hold = false;
+            }
+        }
+    }
     socket.in.note = (e)=>{
         let [channel, pitch, velocity] = e;
-        // if(!velocity) return;
+        if(!velocity){
+            if(colours.hold){
+                return;
+            }
+        }
         let voiceArray = voices.update(pitch, velocity, true);
         // console.log(voiceArray.map(v=>v.pitch));
         voiceArray.forEach((v,i)=>{
-            // todo: check if pitch has changed to save on writes
+            
           writeNoteColour(v.pitch % 12, i);
         });
         document.querySelectorAll('.testBar').forEach((x,i)=>{
